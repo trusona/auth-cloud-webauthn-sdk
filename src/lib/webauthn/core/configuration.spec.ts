30759dc61780e94f870dc19f55146e08bb9c2e5c
Nikolas Thitu <nikolas@hauz.io>
2023-03-02 11:22:49 -0500

    spec

---
+++ src/lib/webauthn/core/configuration.spec.ts
@@ -0,0 +1,46 @@
+
+import { Initializer } from './configuration'
+
+describe('Configuration', () => {
+  describe('#initialize', () => {
+    describe('when a valid URL is provided', () => {
+      beforeEach(() => {
+        // @ts-expect-error
+        global.fetch = jest.fn(async () =>
+          await Promise.resolve({
+            headers: {},
+            status: 200,
+            ok: true,
+            json: async () => await Promise.resolve({ clientId: 'clientId' })
+          }))
+      })
+
+      it('will initialize a valid configuration', async () => {
+        await Initializer.initialize('http://localhost')
+        expect(Initializer.configuration).toBeTruthy()
+      })
+
+      it('will initialize a clientId value', async () => {
+        await Initializer.initialize('http://localhost')
+        expect(Initializer.configuration?.clientId).toBe('clientId')
+      })
+    })
+
+    describe('when an invalid URL is provided', () => {
+      beforeEach(() => {
+        // @ts-expect-error
+        global.fetch = jest.fn(async () =>
+          await Promise.resolve({
+            headers: {},
+            status: 404,
+            ok: false,
+            json: () => Promise.resolve({ })
+          }))
+      })
+
+      it('will throw an error', async () => {
+        expect(await Initializer.initialize('http://localhost')).toThrow()
+      })
+    })
+  })
+})
