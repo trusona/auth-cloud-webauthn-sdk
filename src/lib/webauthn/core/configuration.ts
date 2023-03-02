f56ae4412b5855c373bdb34eef567a22fdb99976
Nikolas Thitu <nikolas@hauz.io>
2023-03-02 10:03:50 -0500

    load configuration

--- src/lib/webauthn/core/configuration.ts
+++ src/lib/webauthn/core/configuration.ts
@@ -2,23 +2,36 @@
 export interface Configuration {
   tenantUrl: string
   clientId: string
-
-  // more urls
+  // more data ???
 }
 
 export class Initializer {
-  private static config: Configuration
+  private static config?: Configuration
 
-  static initialize (tenantUrl: string) {
-    // make requests to well known for other URLs, plus client ID
-    // does a bunch of things and sets configuration
+  static async initialize (tenantUrl: string): Promise<void> {
+    this.config = await this.loadConfiguration(tenantUrl)
 
-    //
-
-    this.config = { tenantUrl: '', clientId: '' }
+    if (this.configuration === undefined) {
+      throw new Error('Configuration was not loaded')
+    }
   }
 
-  static get configuration (): Configuration {
+  static get configuration (): Configuration | undefined {
     return this.config
+  }
+
+  private static async loadConfiguration (tenantUrl: string): Promise<Configuration | undefined> {
+    const response = await fetch(`${tenantUrl}/configuration`)
+
+    if (response.ok) {
+      const map = await response.json()
+
+      return await Promise.resolve({
+        clientId: map.clientId,
+        tenantUrl
+      })
+    } else {
+      return await Promise.resolve(undefined)
+    }
   }
 }
