de0cc96ee26805107f59521acb8f2ecf6e385a85
Nikolas Thitu <nikolas@hauz.io>
2023-03-02 09:13:38 -0500

    update spec

--- src/lib/webauthn/preflight/preflight-checks.spec.ts
+++ src/lib/webauthn/preflight/preflight-checks.spec.ts
@@ -18,17 +18,17 @@ describe('DefaultPreflightChecks', () => {
 
   describe('#isSupported', () => {
     test.each`
-    mediation | authenticator |expected
-    ${true}   | ${true}       |${true}
-    ${false}  | ${true}       |${false}
-    ${true}   | ${false}      |${false}
-    ${false}  | ${false}      |${false}
+    mediation           | authenticator       | expected
+    ${'available'}      | ${'available'}      | ${true}
+    ${'not available'}  | ${'available'}      | ${false}
+    ${'available'}      | ${'not available'}  | ${false}
+    ${'not available'}  | ${'not available'}  | ${false}
     `
     ('returns $expected when conditional mediation is $mediation and a platform authenticator is $authenticator', async ({ mediation, authenticator, expected }) => {
       spy.mockImplementation(() => ({
         PublicKeyCredential: {
-          isConditionalMediationAvailable: async () => { return await Promise.resolve(mediation) },
-          isUserVerifyingPlatformAuthenticatorAvailable: async () => { return await Promise.resolve(authenticator) }
+          isConditionalMediationAvailable: async () => { return await Promise.resolve(mediation === 'available') },
+          isUserVerifyingPlatformAuthenticatorAvailable: async () => { return await Promise.resolve(authenticator === 'available') }
         }
       }))
 
