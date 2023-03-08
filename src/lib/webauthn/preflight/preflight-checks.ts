521d7b507d70f0242d841e21976f11b26ea250ca
Nikolas Thitu <nikolas@hauz.io>
2023-03-08 16:35:44 -0500

    static preflight method

--- src/lib/webauthn/preflight/preflight-checks.ts
+++ src/lib/webauthn/preflight/preflight-checks.ts
@@ -3,12 +3,20 @@ export interface PreflightChecks {
 }
 
 export class DefaultPreflightChecks implements PreflightChecks {
+  static async supported (): Promise<boolean> {
+    return await new DefaultPreflightChecks().isSupported()
+  }
+
   async isSupported (): Promise<boolean> {
-    const value: boolean = typeof window.PublicKeyCredential !== 'undefined' &&
+    try {
+      const value: boolean = typeof window.PublicKeyCredential !== 'undefined' &&
     (await this.isUserVerifyingPlatformAuthenticatorAvailable())?.valueOf() &&
     (await this.isConditionalMediationAvailable())?.valueOf()
 
-    return await Promise.resolve(value)
+      return await Promise.resolve(value)
+    } catch (e) {
+      return await Promise.reject(new Error('PublicKeyCredential support not found'))
+    }
   }
 
   private async isConditionalMediationAvailable (): Promise<boolean> {
