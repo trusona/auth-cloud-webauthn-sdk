9da2b0124f9a60e3e04a10ae2412878d6e07646e
Nikolas Thitu <nikolas@hauz.io>
2023-10-27 16:17:25 -0400

    add credential flag map

--- src/lib/webauthn/core/passkey-management.ts
+++ src/lib/webauthn/core/passkey-management.ts
@@ -7,6 +7,14 @@ export interface PassKey {
   userIdentifier: string
 }
 
+export enum CredentialFlag {
+  BACKUP_STATE= 'BACKUP_STATE',
+  USER_PRESENT= 'USER_PRESENT',
+  USER_VERIFIED= 'USER_VERIFIED',
+  EXTENSION_DATA= 'EXTENSION_DATA',
+  BACKUP_ELIGIBILITY= 'BACKUP_ELIGIBILITY'
+}
+
 export enum CredentialActivityType {
   REGISTRATION = 'REGISTRATION',
   ASSERTION = 'ASSERTION',
@@ -22,6 +30,7 @@ export interface PassKeyActivity {
   ipAddress: string
   operatingSystem: string
   createdAt: Date
+  credentialFlags:Map<CredentialFlag, boolean>
 }
 
 export interface PassKeyManagement {
